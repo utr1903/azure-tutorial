0303a8a1348f69878438613322091abd0062b062
uturkarslan <ugurturkarslan@gmail.com>
2022-05-22 15:00:23 +0200

    Add device

---
+++ iot-tutorial/devices/device01/main.js
@@ -0,0 +1,88 @@
+// Main script
+
+// Import libraries.
+const Protocol = require('azure-iot-device-mqtt').Mqtt;
+const { Client, Message } = require('azure-iot-device');
+
+// Set variables.
+const deviceId = `device01`;
+
+// IoT Hub
+let client;
+let sendInterval;
+
+function disconnectHandler () {
+    clearInterval(sendInterval);
+
+    sendInterval = null;
+
+    client.open().catch((err) => {
+        console.error(err.message);
+    });
+}
+
+function messageHandler (msg) {
+    console.log('Id: ' + msg.messageId + ' Body: ' + msg.data);
+    client.complete(msg, printResultFor('completed'));
+}
+
+function generateMessage () {
+
+    const windSpeed = 10 + (Math.random() * 4); // range: [10, 14]
+    const temperature = 20 + (Math.random() * 10); // range: [20, 30]
+    const humidity = 60 + (Math.random() * 20); // range: [60, 80]
+
+    const data = JSON.stringify({
+        sensorName: deviceId,
+        windSpeed: windSpeed,
+        temperature: temperature,
+        humidity: humidity
+    });
+
+    const message = new Message(data);
+
+    return message;
+}
+
+function errorHandler (err) {
+    console.error(err.message);
+}
+
+function connectHandler () {
+    console.log('Client connected');
+
+    // Create a message and send it to the IoT Hub every two seconds
+    if (!sendInterval) {
+        sendInterval = setInterval(() => {
+            const message = generateMessage();
+            console.log('Sending message: ' + message.getData());
+            client.sendEvent(message, printResultFor('send'));
+        }, 1000);
+    }
+}
+
+function printResultFor(op) {
+    return function printResult(err, res) {
+        if (err) console.log(op + ' error: ' + err.toString());
+        if (res) console.log(op + ' status: ' + res.constructor.name);
+    };
+}
+
+async function main() {
+
+    // Get Service Bus credentials.
+    const deviceConnectionString = "";
+
+    client = Client.fromConnectionString(deviceConnectionString, Protocol);
+    client.on('connect', connectHandler);
+    client.on('error', errorHandler);
+    client.on('disconnect', disconnectHandler);
+    client.on('message', messageHandler);
+
+    client.open()
+        .catch(err => {
+        console.error('Could not connect: ' + err.message);
+    });
+};
+
+main();
