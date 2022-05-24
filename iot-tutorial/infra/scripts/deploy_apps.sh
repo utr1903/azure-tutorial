8d9c878b9fc98ea5556350d60a9cc82d1790eee7
uturkarslan <ugurturkarslan@gmail.com>
2022-05-24 19:55:44 +0200

    Add ingress nginx

--- iot-tutorial/infra/scripts/deploy_apps.sh
+++ iot-tutorial/infra/scripts/deploy_apps.sh
@@ -34,7 +34,7 @@ iproc["namespace"]="iproc"
 ### Build & Push
 
 # InputProcessor
-echo -e "\n--- InputProcessor ---\n"
+echo -e "\n--- Input Processor ---\n"
 docker build \
     --tag "${DOCKERHUB_NAME}/${iproc[name]}" \
     ../../apps/InputProcessor/InputProcessor/.
@@ -42,6 +42,35 @@ docker push "${DOCKERHUB_NAME}/${iproc[name]}"
 echo -e "\n------\n"
 
 ### Helm
+
+# Ingress Controller
+echo "Deploying Ingress Controller ..."
+
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx && \
+helm repo update; \
+helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx \
+    --namespace nginx --create-namespace \
+    --wait \
+    --debug \
+    --set controller.replicaCount=1 \
+    --set controller.nodeSelector."kubernetes\.io/os"="linux" \
+    --set controller.image.image="ingress-nginx/controller" \
+    --set controller.image.tag="v1.1.1" \
+    --set controller.image.digest="" \
+    --set controller.service.externalTrafficPolicy=Local \
+    --set controller.admissionWebhooks.patch.nodeSelector."kubernetes\.io/os"="linux" \
+    --set controller.admissionWebhooks.patch.image.image="ingress-nginx/kube-webhook-certgen" \
+    --set controller.admissionWebhooks.patch.image.tag="v1.1.1" \
+    --set controller.admissionWebhooks.patch.image.digest="" \
+    --set defaultBackend.nodeSelector."kubernetes\.io/os"="linux" \
+    --set defaultBackend.image.image="defaultbackend-amd64" \
+    --set defaultBackend.image.tag="1.5" \
+    --set defaultBackend.image.digest=""
+
+echo -e " -> Ingress Controller is successfully deployed.\n"
+
+# InputProcessor
+echo "Deploying Input Processor ..."
 
 serviceBusConnectionString=$(az servicebus namespace authorization-rule keys list \
   --resource-group $resourceGroupName \
@@ -55,9 +84,6 @@ eventHubConnectionString=$(az eventhubs namespace authorization-rule keys list \
   --name "RootManageSharedAccessKey" \
   | jq .primaryConnectionString)
 
-# InputProcessor
-echo "Deploying InputProcessor ..."
-
 helm upgrade ${iproc[name]} \
   --install \
   --wait \
@@ -70,3 +96,5 @@ helm upgrade ${iproc[name]} \
   --set eventHubConnectionString=$eventHubConnectionString \
   --set eventHubName=$eventHubName \
   ../charts/InputProcessor
+
+echo -e " -> Input Processor is successfully deployed.\n"
