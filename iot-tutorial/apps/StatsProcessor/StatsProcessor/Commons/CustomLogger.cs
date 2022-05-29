a1f63e31525350b2b9aa207b9e0a02fcdcd0561e
uturkarslan <ugurturkarslan@gmail.com>
2022-05-29 13:50:08 +0200

    Change Stats Processor

---
+++ iot-tutorial/apps/StatsProcessor/StatsProcessor/Commons/CustomLogger.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace StatsProcessor.Commons
+{
+    public static class CustomLogger
+    {
+        public static void Log(
+            ILogger logger,
+            LogLevel logLevel,
+            string className,
+            string methodName,
+            string message,
+            string deviceName = null,
+            string value = null
+        )
+        {
+            var log = CreateLogMessage(logLevel, className, methodName, message, deviceName, value);
+
+            switch (logLevel)
+            {
+                case LogLevel.Information:
+                    logger.LogInformation(log);
+                    break;
+
+                case LogLevel.Debug:
+                    logger.LogDebug(log);
+                    break;
+
+                case LogLevel.Warning:
+                    logger.LogWarning(log);
+                    break;
+
+                case LogLevel.Error:
+                    logger.LogError(log);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private static string CreateLogMessage(
+            LogLevel logLevel,
+            string className,
+            string methodName,
+            string message,
+            string deviceName,
+            string value
+        )
+        {
+            var log = $"logLevel:{logLevel}," +
+                $"className:{className}," +
+                $"methodName:{methodName}," +
+                $"message:{message}";
+
+            if (deviceName != null)
+                log += $",deviceName:{deviceName}";
+
+            if (value != null)
+                log += $",value:{value}";
+
+            return log;
+        }
+    }
+}
