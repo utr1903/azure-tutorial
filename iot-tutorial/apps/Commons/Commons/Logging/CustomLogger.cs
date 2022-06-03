cc7037821a2f7fc91f5aef7f6f81a206ecfba346
uturkarslan <ugurturkarslan@gmail.com>
2022-06-03 21:31:49 +0200

    Add Commons library

---
+++ iot-tutorial/apps/Commons/Commons/Logging/CustomLogger.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Commons.Logging
+{
+    public static class CustomLogger
+    {
+        public static void Log(
+            ILogger logger,
+            LogLevel logLevel,
+            string className,
+            string methodName,
+            string message,
+            string data = null,
+            string exception = null
+        )
+        {
+            var log = CreateLogMessage(logLevel, className, methodName, message, data, exception);
+
+            switch (logLevel)
+            {
+                case LogLevel.Information:
+                    logger.LogInformation(log);
+                    break;
+
+                case LogLevel.Debug:
+                    logger.LogDebug(log);
+                    break;
+
+                case LogLevel.Warning:
+                    logger.LogWarning(log);
+                    break;
+
+                case LogLevel.Error:
+                    logger.LogError(log);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private static string CreateLogMessage(
+            LogLevel logLevel,
+            string className,
+            string methodName,
+            string message,
+            string data,
+            string exception
+        )
+        {
+            var log = $"logLevel:{logLevel}," +
+                $"className:{className}," +
+                $"methodName:{methodName}," +
+                $"message:{message}";
+
+            if (data != null)
+                log += $",data:{data}";
+
+            if (exception != null)
+                log += $",exception:{exception}";
+
+            return log;
+        }
+    }
+}
